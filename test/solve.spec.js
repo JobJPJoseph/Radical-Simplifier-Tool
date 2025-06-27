aa39c5d7f0c72040defaa0d0953c698b8f641073
Job Joseph <jobjpjoseph@gmail.com>
2025-06-27 08:48:41 -0400

    we updated the solve.spec

--- test/solve.spec.js
+++ test/solve.spec.js
@@ -5,6 +5,7 @@ const spies = require('chai-spies');
 chai.use(spies);
 
 const { Solve } = require('../lib/solve');
+const { Radicals } = require('../lib/interface');
 
 describe('Solve', function () {
 
@@ -12,9 +13,39 @@ describe('Solve', function () {
         expect(Solve).to.exist;
     });
 
-    describe('Construction', function () {
+    let solve;
+
+    before(function () {
+        solve = new Solve();
+    });
+
+    describe('construction', function () {
 
         it('should initialize the interface class', function () {
+            expect(solve.interface).to.be.an.instanceOf(Radicals);
+        });
+
+    });
+
+    describe('Convert', function () {
+
+        it('should a string input into a number', function () {
+
+        });
+
+    });
+
+    describe('Factors', function () {
+
+        it('should return a 2-D array where each indices represents [factor, divisor]', function () {
+
+        });
+
+    });
+
+    describe('SolveExpression', function () {
+
+        it('should retrieve the users input and return the result of the expression if applicable.', function () {
 
         });
 
